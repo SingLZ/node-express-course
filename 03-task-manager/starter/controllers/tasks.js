9d50fda9c7bf0678157e804ed6e8bea3bdf8ca7d
SingLZ <0lix.ing0@gmail.com>
2025-04-22 22:49:37 -0400

    Complete Week 6

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,25 +1,54 @@
 const Task = require('../models/Task.js');
+const asyncWrapper = require('../middleware/async.js');
+const { createCustomError } = require('../errors/custom-error.js');
 
-const getAllTasks = (req, res) => {
-    res.send('all items from file');
+const getAllTasks = asyncWapper(async (req, res) => {
+        const tasks = await Task.find({});
+        //res.status(200).json({tasks, amount:tasks.length});
+        res.status(200).json({tasks});
+    
+
+})
+
+const createTasks = asyncWrapper(async (req, res) => {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+})
+
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({_id: taskID});
+        if (!task) {
+            return next(createCustomError(`No task with id : ${taskID}`, 404));
+        }
+    } catch (error) {
+        res.status(500).json({msg: error});
+    } 
 }
 
-const createTasks = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({task});
-}
+const updateTasks = asyncWrapper (async (req, res) => {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task ) {
+            return res.status(404).json({msg: `No task with id : ${taskID}`});
+        }
+        res.status(200).json({task});
+    
+})
 
-const getTask = (req, res) => {
-    res.send('get single task');
-}
-
-const updateTasks = (req, res) => {
-    res.send('update task');
-}
-
-const delateTasks = (req, res) => {
-    res.send('delete task');
-}
+const delateTasks = asyncWrapper (async (req, res) => {
+    
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({_id: taskID});
+        if (!task) {
+            return res.status(404).json({msg: `No task with id : ${taskID}`});
+        }
+        res.status(200).json({task});
+})
 module.exports = {
     getAllTasks,
     createTasks,
